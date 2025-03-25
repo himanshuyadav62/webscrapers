efe5244f331f6b34f38dba357646c697544ada51
Himanshu yadav <119734736+himanshuyadav62@users.noreply.github.com>
2025-03-25 17:22:23 +0530

    empty commit for initial setup

--- nykaa/app.py
+++ nykaa/app.py
@@ -13,6 +13,8 @@ def configure_browser():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("window-size=1920,1080")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--ignore-certificate-errors")  # Ignore SSL errors
+    chrome_options.add_argument("--allow-running-insecure-content")
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
     )
